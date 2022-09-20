5d62e27395435c52ff3021315fc64004b750b92e
GanangPrakoso <ganangmanggolo@gmail.com>
2022-09-20 10:42:24 +0700

    change routers

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,6 @@ const router = require("express").Router();
 const userRouter = require("./users");
 const movieRouter = require("./movies");
 
-router.get("/", (req, res) => {
-  res.send("halooooo");
-});
-
 router.use("/users", userRouter);
 
 router.use("/movies", movieRouter);
