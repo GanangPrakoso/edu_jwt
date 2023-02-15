beaaf2d9a26862a05f57b4b38cd54974db52b3b7
GanangPrakoso <ganangmanggolo@gmail.com>
2023-02-16 02:10:21 +0700

    change config port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const express = require("express");
 const errorHandler = require("./middlewares/errorHandler");
 const router = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 const cors = require("cors");
 
 app.use(cors());
