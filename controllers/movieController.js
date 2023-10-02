54fec13da3c1ec8eac0636f838b721e7d50b57a9
GanangPrakoso <ganangmanggolo@gmail.com>
2023-10-03 01:06:51 +0700

    change catch err parameter

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -42,8 +42,7 @@ class MovieController {
       });
 
       res.status(200).json({ message: `movie with id ${id} has been deleted` });
-    } catch (error) {
-      console.log(error, "<<<<");
+    } catch (err) {
       res.status(500).json({ message: "Internal Server Error" });
     }
   }
