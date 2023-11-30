95c21c28d274f0ba5fe0bddb73cc7926dbbca03b
GanangPrakoso <ganangmanggolo@gmail.com>
2023-11-30 13:48:11 +0700

    back to before lecture

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+require("dotenv").config();
 
 const express = require("express");
 const errorHandler = require("./middlewares/errorHandler");
 const router = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 const cors = require("cors");
 
 app.use(cors());
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const movieRouter = require("./movies");
 const authentication = require("../middlewares/authentication");
 
 router.get("/", (req, res) => res.send("halo aku terkoneksi dengan baik"));
+
 router.use("/users", userRouter);
 
 // authentication
