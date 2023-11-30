9385a6d9343fd99789e874bb0a340f2e7ae7d91f
GanangPrakoso <ganangmanggolo@gmail.com>
2023-11-30 09:53:38 +0700

    feat: ready to deploy

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const userRouter = require("./users");
 const movieRouter = require("./movies");
 const authentication = require("../middlewares/authentication");
 
+router.get("/", (req, res) => res.send("halo aku terkoneksi dengan baik"));
 router.use("/users", userRouter);
 
 // authentication
